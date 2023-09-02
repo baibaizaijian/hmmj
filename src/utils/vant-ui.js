f421fac4fd961478bcf9b76f478886b26c477953
bzz <3024131925@qq.com>
2023-09-02 11:23:44 +0800

    注册静态

--- src/utils/vant-ui.js
+++ src/utils/vant-ui.js
@@ -1,8 +1,11 @@
 
 import Vue from 'vue'
-import { Button, Tabbar, TabbarItem } from 'vant'
+import { Button, Tabbar, TabbarItem, NavBar, Form, Field } from 'vant'
 
 // 按需引入 Button 组件
 Vue.use(Button)
 Vue.use(Tabbar)
 Vue.use(TabbarItem)
+Vue.use(NavBar)
+Vue.use(Form)
+Vue.use(Field)
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,6 +1,27 @@
 <template>
   <div class="register-page">
-    注册页
+    <van-nav-bar title="注册" />
+    <van-form @submit="onSubmit">
+      <van-field
+        v-model="username"
+        name="username"
+        label="用户名"
+        placeholder="请输入用户名"
+        :rules="[{ required: true, message: '请填写用户名' }]"
+      />
+      <van-field
+        v-model="password"
+        type="password"
+        name="password"
+        label="密码"
+        placeholder="请输入密码"
+        :rules="[{ required: true, message: '请填写密码' }]"
+      />
+      <div style="margin: 16px">
+        <van-button block type="primary" native-type="submit">注册</van-button>
+      </div>
+    </van-form>
+    <router-link to="/login" class="link">有账号,去登录</router-link>
   </div>
 </template>
 
@@ -9,11 +30,14 @@ export default {
   name: 'RegisterPage',
   data () {
     return {
-
+      username: '',
+      password: ''
     }
   },
   methods: {
-
+    onSubmit (values) {
+      console.log('submit', values)
+    }
   }
 }
 </script>
